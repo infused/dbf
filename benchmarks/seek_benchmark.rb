0d9e18d00393d418f4064c4def03a1244ce65d3e
infused <infused@6ed3bb0e-e019-0410-903f-c63d4471f3c2>
2007-05-23 05:42:53 +0000

    Clean up benchmarks
    git-svn-id: svn+ssh://rubyforge.org/var/svn/dbf/trunk@54 6ed3bb0e-e019-0410-903f-c63d4471f3c2

--- benchmarks/seek_benchmark.rb
+++ benchmarks/seek_benchmark.rb
@@ -3,12 +3,16 @@ require 'benchmark'
 $:.unshift(File.dirname(__FILE__) + "/../lib/")
 require 'dbf'
 
-iterations = 5000
+puts
+puts "Runs 5000 random row seeks first using the I/O based record(n) method and then using"
+puts "using the array of records."
+puts
 
-Benchmark.bm() do |x|
+iterations = 5000
+Benchmark.bm(20) do |x|
   @dbf = DBF::Reader.new(File.join(File.dirname(__FILE__), '..', 'test', 'databases', 'foxpro.dbf'))
   max = @dbf.record_count + 1
   
-  x.report("  Record seek:") { iterations.times { @dbf.record(rand(max)) } }
-  x.report("Records array:") { iterations.times { @dbf.records[rand(max)] } }
+  x.report("I/O based record(n)") { iterations.times { @dbf.record(rand(max)) } }
+  x.report("array of records[n]") { iterations.times { @dbf.records[rand(max)] } }
 end
