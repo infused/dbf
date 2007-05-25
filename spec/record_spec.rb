803117e3843224131279b3a0cac7598ebff38fd4
infused <infused@6ed3bb0e-e019-0410-903f-c63d4471f3c2>
2007-05-25 20:24:38 +0000

    Spec stub
    git-svn-id: svn+ssh://rubyforge.org/var/svn/dbf/trunk@61 6ed3bb0e-e019-0410-903f-c63d4471f3c2

---
+++ spec/record_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe DBF::Record, "when initialized" do
+  
+  it "should convert number fields with 0 decimals to integers" do
+    
+  end
+  
+  
+end
