bf1821c27ceb127d1c570ed8b46dbd2ac4049234
Keith Morrison <keithm@infused.org>
2008-08-08 16:59:46 -0700

    Allow for missing memo files

--- lib/dbf/record.rb
+++ lib/dbf/record.rb
@@ -56,6 +56,7 @@ module DBF
           unpack_string(column).strip
         end
         @attributes[underscore(column.name)] = @attributes[column.name]
+        @attributes
       end
     end
   
@@ -80,7 +81,7 @@ module DBF
     end
   
     def read_memo(start_block)
-      return nil if start_block < 1
+      return nil if !@table.has_memo_file? || start_block < 1
 
       @table.memo_file_format == :fpt ? build_fpt_memo(start_block) : build_dbt_memo(start_block)
     end
