3582c4705e38300139f0a2d1f037ddafb07ded94
infused <infused@6ed3bb0e-e019-0410-903f-c63d4471f3c2>
2008-12-12 04:04:36 +0000

    Add type_case method
    git-svn-id: svn+ssh://rubyforge.org/var/svn/dbf/trunk@170 6ed3bb0e-e019-0410-903f-c63d4471f3c2

--- lib/dbf/column.rb
+++ lib/dbf/column.rb
@@ -9,6 +9,34 @@ module DBF
       @name, @type, @length, @decimal = strip_non_ascii_chars(name), type, length, decimal
     end
     
+    def type_cast(value)
+      case column.type
+      when 'N' # number
+        column.decimal.zero? ? value.to_i : value.to_f
+      when 'D' # date
+        raw.to_time unless raw.blank?
+      when 'M' # memo
+        starting_block = value.to_i
+        # read_memo(starting_block)
+      when 'L' # logical
+        value.strip =~ /^(y|t)$/i ? true : false
+      when 'I' # integer
+        value.unpack('v').first
+      when 'T' # datetime
+        unpack_datetime(value)
+      else
+        value.to_s.strip
+      end
+    end
+    
+    def unpack_datetime(value)
+      days, milliseconds = value.unpack('l2')
+      hours = (milliseconds / MS_PER_HOUR).to_i
+      minutes = ((milliseconds - (hours * MS_PER_HOUR)) / MS_PER_MINUTE).to_i
+      seconds = ((milliseconds - (hours * MS_PER_HOUR) - (minutes * MS_PER_MINUTE)) / MS_PER_SECOND).to_i
+      DateTime.jd(days, hours, minutes, seconds)
+    end
+    
     def schema_definition
       data_type = case type
       when "N" # number
