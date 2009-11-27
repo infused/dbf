3a98867f220120c7a47c5671ea9f898665e6821f
Keith Morrison <keithm@infused.org>
2009-11-26 22:48:20 -0800

    Implement non-enumerable finder

--- lib/dbf/table.rb
+++ lib/dbf/table.rb
@@ -51,7 +51,7 @@ module DBF
     # Returns the DBF::Record at the specified index
     def record(index)
       seek_to_record(index)
-      DBF::Record.new(self)
+      deleted_record? ? nil : DBF::Record.new(self)
     end
     
     alias_method :row, :record
@@ -105,6 +105,50 @@ module DBF
       end
     end
     
+    # Find records using a simple ActiveRecord-like syntax.
+    #
+    # Examples:
+    #   table = DBF::Table.new 'mydata.dbf'
+    #   
+    #   # Find record number 5
+    #   table.find(5)
+    #
+    #   # Find all records for Keith Morrison
+    #   table.find :all, :first_name => "Keith", :last_name => "Morrison"
+    # 
+    #   # Find first record
+    #   table.find :first, :first_name => "Keith"
+    #
+    # The <b>command</b> can be an id, :all, or :first.
+    # <b>options</b> is optional and, if specified, should be a hash where the keys correspond
+    # to column names in the database.  The values will be matched exactly with the value
+    # in the database.  If you specify more than one key, all values must match in order 
+    # for the record to be returned.  The equivalent SQL would be "WHERE key1 = 'value1'
+    # AND key2 = 'value2'".
+    def find(command, options = {})
+      # results = options.empty? ? records : records.select {|record| all_values_match?(record, options)}
+
+      case command
+      when Fixnum
+        record(command)
+      when :all
+        results = []
+        each do |record|
+          results << record if all_values_match?(record, options)
+        end
+        results
+      when :first
+        each do |record|
+          return record if all_values_match?(record, options)
+        end
+        nil
+      end
+    end
+
+    def all_values_match?(record, options)
+      options.all? {|key, value| record.attributes[key.to_s.underscore] == value}
+    end
+    
     private
     
     def open_memo(file)
@@ -123,12 +167,7 @@ module DBF
     end
   
     def deleted_record?
-      if @data.read(1).unpack('a') == ['*']
-        @data.rewind
-        true
-      else
-        false
-      end
+      @data.read(1).unpack('a') == ['*']
     end
   
     def get_header_info
--- spec/unit/table_spec.rb
+++ spec/unit/table_spec.rb
@@ -97,6 +97,90 @@ describe DBF::Table do
       File.exists?('test.csv').should be_true
     end
   end
+  
+  describe "#record" do
+    before do
+      @table = DBF::Table.new "#{DB_PATH}/dbase_83.dbf"
+      
+      @records = []
+      @table.each {|record| @records << record}
+    end
+    
+    it "should nullify deleted records" do
+      @table.stub!(:deleted_record?).and_return(true)
+      @table.record(5).should be_nil
+    end
+  end
+  
+  describe "#find" do
+    describe "with index" do
+      before do
+        @table = DBF::Table.new "#{DB_PATH}/dbase_83.dbf"
+      end
+      
+      it "should return the correct record" do
+        @table.find(5).should == @table.record(5)
+      end
+    end
+    
+    describe "with :all" do
+      before do
+        @table = DBF::Table.new "#{DB_PATH}/dbase_83.dbf"
+        
+        @records = []
+        @table.each {|record| @records << record}
+      end
+
+      it "should return all records if options are empty" do
+        @table.find(:all).should == @records
+      end
+
+      it "should return matching records when used with options" do
+        @table.find(:all, "WEIGHT" => 0.0).should == @records.select {|r| r.attributes["weight"] == 0.0}
+      end
+
+      it "should AND multiple search terms" do
+        @table.find(:all, "ID" => 30, "IMAGE" => "graphics/00000001/TBC01.jpg").should == []
+      end
+      
+      it "should match original column names" do
+        @table.find(:all, "WEIGHT" => 0.0).should_not be_empty
+      end
+      
+      it "should match symbolized column names" do
+        @table.find(:all, :WEIGHT => 0.0).should_not be_empty
+      end
+      
+      it "should match downcased column names" do
+        @table.find(:all, "weight" => 0.0).should_not be_empty
+      end
+      
+      it "should match symbolized downcased column names" do
+        @table.find(:all, :weight => 0.0).should_not be_empty
+      end
+    end
+    
+    describe "with :first" do
+      before do
+        @table = DBF::Table.new "#{DB_PATH}/dbase_83.dbf"
+        
+        @records = []
+        @table.each {|record| @records << record}
+      end
+
+      it "should return the first record if options are empty" do
+        @table.find(:first).should == @records.first
+      end
+
+      it "should return the first matching record when used with options" do
+        @table.find(:first, "CODE" => "C").should == @records[5]
+      end
+
+      it "should AND multiple search terms" do
+        @table.find(:first, "ID" => 30, "IMAGE" => "graphics/00000001/TBC01.jpg").should be_nil
+      end
+    end
+  end
 
 end
 
