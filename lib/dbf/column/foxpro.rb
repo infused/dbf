088d7815e3a01d8c2dd5ed0b904fbaba8fe3f6fd
Keith Morrison <keithm@infused.org>
2014-06-10 00:45:07 -0700

    comment out unused typecast method

--- lib/dbf/column/foxpro.rb
+++ lib/dbf/column/foxpro.rb
@@ -1,9 +1,9 @@
 module DBF
   module Column
     class Foxpro < Base
-      def unpack_binary(value) #nodoc
-        value.unpack('d')[0]
-      end
+      # def unpack_binary(value) #nodoc
+      #   value.unpack('d')[0]
+      # end
     end
   end
 end
