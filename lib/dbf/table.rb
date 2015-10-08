26d00df769c6d7b183e45ca438547e933dff7bed
Keith Morrison <keithm@infused.org>
2015-10-08 01:21:52 -0700

    test for 0d only

--- lib/dbf/table.rb
+++ lib/dbf/table.rb
@@ -241,11 +241,7 @@ module DBF
       pos = @data.pos
       byte = @data.read(1)
       @data.seek(pos)
-      !printable_ascii_chars.include?(byte[0].ord)
-    end
-
-    def printable_ascii_chars # nodoc
-      32..127
+      byte.ord == 13
     end
 
     def foxpro? # nodoc
