c4990368382572d087a24ad34ee2083c400aefc0
Keith Morrison <keithm@infused.org>
2017-11-22 00:07:48 -0800

    Revert "replace inject with sum"
    This reverts commit 19b00d505413b37375ef38b7b737dce6e796c919.

--- spec/dbf/file_formats_spec.rb
+++ spec/dbf/file_formats_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.shared_examples_for 'DBF' do
   specify 'sum of column lengths should equal record length specified in header plus one' do
     header_record_length = table.instance_eval { @header.record_length }
-    sum_of_column_lengths = table.columns.sum(&:length) + 1
+    sum_of_column_lengths = table.columns.inject(1) { |sum, column| sum += column.length }
 
     expect(header_record_length).to eq sum_of_column_lengths
   end
